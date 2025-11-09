4108f0de96c823ced7b51d1c5e8295386617538d
DmitriyDemyanenko <DemyanenkoDW@ukr.net>
2025-11-09 20:11:19 +0200

    fix: update S3 bucket name in backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "santa-prod-demyanenko"
-    key          = "terraform.tfstate"
+    key          = "terraform/terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
     encrypt      = true
